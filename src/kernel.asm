f3398e4f891c3a7a418e60904864a26e52beaf65
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-03-20 20:02:06 +0100

    Add annotations on the assembly file

--- src/kernel.asm
+++ src/kernel.asm
@@ -2,23 +2,25 @@
 
 ;; This file contains assembly code to run the kernel.
 
-bits 32                   ; NASM directive to produce 32 bit code
-section .text             ; Begin the code (text) section
-  ; multiboot spec
-  align 4
-  dd 0x1BADB002            ;magic
-  dd 0x00                  ;flags
-  dd - (0x1BADB002 + 0x00) ;checksum. m+f+c 
+bits 32                     ; NASM directive to produce 32 bit code
+section .text               ; Begin the code (text) section
+  ; MultiBoot Header
+  align 4                   ; NASM directive that 'align' the next instruction,
+                            ; It will align the next instruction on the next address that is a 4 multiple
+                            ; This instruction is facultative but can improve performance
+  dd 0x1BADB002             ; Magic Number to identify the header
+  dd 0x00                   ; Flags (TODO : see multiple boot specification for further information)
+  dd - (0x1BADB002 + 0x00)  ; CheckSum. m+f+c must be equal to 0
 
-global start              ; NASM directive to set the start symbol as global
-                          ; It is useful for linker to know where the start symbol is
-extern kmain              ; Declare kmain as an external function (declared in kernel.c)
+global start                ; NASM directive to set the start symbol as global
+                            ; It is useful for linker to know where the start symbol is
+extern kmain                ; Declare kmain as an external function (declared in kernel.c)
 
-start:                    ; Start function
-  cli                     ; Disable interruptions (CLear Interrups)
-                          ; Otherwise CPU will be awake by them
-  call kmain              ; Run the kernel
-  hlt                     ; Halt the CPU TODO : Why ?
+start:                      ; Start function
+  cli                       ; Disable interruptions (CLear Interrups)
+                            ; Otherwise CPU will be awake by them
+  call kmain                ; Run the kernel
+  hlt                       ; Halt the CPU TODO : Why ?
 
-section .bss              ; TODO : What is the bss section ?
-resb 8192                 ; Alloc 8KB for the stack
+section .bss                ; TODO : What is the bss section ?
+resb 8192                   ; Alloc 8KB for the stack
