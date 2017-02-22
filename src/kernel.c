3bf0e58c2b3b62f2336f8fb65e333c6fe713c115
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-02-22 22:03:35 +0100

    Refactorize code

--- src/kernel.c
+++ src/kernel.c
@@ -2,9 +2,31 @@
 
 /* The screen is composed of 25 lignes of 80 columns. */
 #define NB_LINES_SCREEN 25
-#define NB_CHAR_PER_LINE 80
+#define NB_CHAR_PER_LINE 60
+#define NB_BYTES_TOTAL (NB_CHAR_PER_LINE * NB_CHAR_PER_LINE)
 
-char *video_ptr = (char *)0xb8000; /* Beginning of the video memory */
+typedef unsigned short int  uint16_t;
+typedef unsigned char       uint8_t;
+typedef unsigned int        size_t;
+
+uint16_t *video_ptr = (uint16_t *) 0xb8000; /* Beginning of the video memory */
+
+size_t  current_row     = 0;
+size_t  current_column  = 0;
+
+/**
+ * This function build a tuple with foreground and background color.
+ */
+static inline uint8_t vga_entry_color() {
+	return 0x0 | 0xF << 4;
+}
+
+/**
+ * This function build a tuple char + color to be printed on screen.
+ */
+static inline uint16_t vga_entry(unsigned char c, uint8_t color) {
+	return (uint16_t) color << 8 | (uint16_t) c;
+}
 
 /**
  *  This function clear the current screen by writing blank characters on it.
@@ -12,31 +34,43 @@ char *video_ptr = (char *)0xb8000; /* Beginning of the video memory */
 void clear_screen()
 {
   unsigned int cpt;
-  /* Each column contains 2 bytes of data : the char to print and their colors */
-  unsigned const int nb_byte_total = 2 * NB_LINES_SCREEN * NB_CHAR_PER_LINE;
 
-  for(cpt = 0 ; cpt < nb_byte_total ; cpt += 2)
+  current_row     = 0;
+  current_column  = 0;
+
+  for (cpt = 0 ; cpt < NB_BYTES_TOTAL ; cpt += 1)
   {
-    *(video_ptr + cpt)     = ' ';    /* Write a blank character */
-    *(video_ptr + cpt + 1) = 0xF0;   /* Write it black on white screen */
+    *(video_ptr + cpt) = vga_entry(' ', vga_entry_color());
   }
 }
 
 /**
  *  This function print the 'text' string onto the screen.
  */
-void write_string_on_screen(char *text)
+void write_string(char *text)
 {
   unsigned int cpt;
+  char c;
   cpt = 0;
 
   /* We go through the text to print */
-  while(*(text + cpt) != '\0')
+  while ((c = *(text + cpt)) != '\0')
   {
-    *(video_ptr + (2 *cpt))      = *(text + cpt);
-    *(video_ptr + (2 * cpt) + 1)  = 0xF0;
+    if (c == '\n') {
+      current_row ++;
+      cpt ++;
+      continue;
+    }
 
+    *(video_ptr + (current_column + NB_CHAR_PER_LINE * current_row)) = vga_entry(c, vga_entry_color());
+
+    current_column ++;
     cpt ++;
+
+    if (current_column == NB_CHAR_PER_LINE) {
+      current_row ++;
+      current_column = 0;
+    }
   }
 }
 
@@ -47,6 +81,6 @@ void write_string_on_screen(char *text)
 void kmain(void)
 {
   clear_screen();
-  write_string_on_screen("Welcome to my Kernel");
+  write_string("Welcome to my Kernel\nI'm very happy to see you !");
   return;
 }
